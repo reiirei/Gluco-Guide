025bffc7809040008fd6d029693af92f5d785b48
Restu Gede Purnama <restumelenoy83@gmail.com>
2024-12-05 03:10:58 +0700

    modify/remove unnecessary field for datastore

--- MD/app/src/main/java/com/health/glucoguide/data/local/UserPreference.kt
+++ MD/app/src/main/java/com/health/glucoguide/data/local/UserPreference.kt
@@ -19,9 +19,7 @@ class UserPreference @Inject constructor(@ApplicationContext context: Context) {
 
     suspend fun saveSession(user: UserSession) {
         dataStore.edit { preferences ->
-            preferences[USER_EMAIL] = user.email
             preferences[USER_NAME] = user.name
-            preferences[USER_PASSWORD] = user.password
             preferences[USER_TOKEN] = user.token ?: ""
             preferences[USER_IS_LOGIN] = user.isLogin
         }
@@ -30,9 +28,7 @@ class UserPreference @Inject constructor(@ApplicationContext context: Context) {
     fun getSession(): Flow<UserSession> {
         return dataStore.data.map { preferences ->
             UserSession(
-                preferences[USER_EMAIL] ?: "",
                 preferences[USER_NAME] ?: "",
-                preferences[USER_PASSWORD] ?: "",
                 preferences[USER_TOKEN] ?: "",
                 preferences[USER_IS_LOGIN] ?: false
             )
@@ -46,9 +42,7 @@ class UserPreference @Inject constructor(@ApplicationContext context: Context) {
     }
 
     companion object {
-        private val USER_EMAIL = stringPreferencesKey("email")
         private val USER_NAME = stringPreferencesKey("name")
-        private val USER_PASSWORD = stringPreferencesKey("password")
         private val USER_TOKEN = stringPreferencesKey("token")
         private val USER_IS_LOGIN = booleanPreferencesKey("isLogin")
     }
