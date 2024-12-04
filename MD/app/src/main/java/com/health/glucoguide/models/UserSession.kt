13940f8376db4f19bbc837e652a56fa4ed029a08
Restu Gede Purnama <restumelenoy83@gmail.com>
2024-12-04 12:31:25 +0700

    modify/added field password and token into model

--- MD/app/src/main/java/com/health/glucoguide/models/UserSession.kt
+++ MD/app/src/main/java/com/health/glucoguide/models/UserSession.kt
@@ -3,6 +3,7 @@ package com.health.glucoguide.models
 data class UserSession(
     val email: String,
     val name: String,
-    val token: String,
+    val password: String,
+    val token: String? = "",
     val isLogin: Boolean = false
 )
