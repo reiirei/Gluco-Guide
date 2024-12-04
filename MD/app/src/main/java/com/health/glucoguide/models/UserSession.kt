ee9e48271f75537f2942702cf6b4ac6ca9408e2f
Restu Gede Purnama <restumelenoy83@gmail.com>
2024-12-05 03:08:13 +0700

    modify/remove unnecessary field

--- MD/app/src/main/java/com/health/glucoguide/models/UserSession.kt
+++ MD/app/src/main/java/com/health/glucoguide/models/UserSession.kt
@@ -1,9 +1,7 @@
 package com.health.glucoguide.models
 
 data class UserSession(
-    val email: String,
     val name: String,
-    val password: String,
     val token: String? = "",
     val isLogin: Boolean = false
 )
