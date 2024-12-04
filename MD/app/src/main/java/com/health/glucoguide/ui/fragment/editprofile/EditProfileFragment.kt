d5d7b10f1225e2ff55f34840cffd4fdfdd459b68
Restu Gede Purnama <restumelenoy83@gmail.com>
2024-12-04 12:27:53 +0700

    feature/implement api postuserdata

--- MD/app/src/main/java/com/health/glucoguide/ui/fragment/editprofile/EditProfileFragment.kt
+++ MD/app/src/main/java/com/health/glucoguide/ui/fragment/editprofile/EditProfileFragment.kt
@@ -5,56 +5,127 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.health.glucoguide.R
+import com.health.glucoguide.data.ResultState
+import com.health.glucoguide.databinding.FragmentEditProfileBinding
+import com.health.glucoguide.models.UserSession
+import com.health.glucoguide.util.ProgressDialogUtil
+import dagger.hilt.android.AndroidEntryPoint
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [EditProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+@AndroidEntryPoint
 class EditProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentEditProfileBinding
+    private val viewModel: EditProfileViewModel by viewModels()
+    private lateinit var userData: UserSession
+    private val progressDialog by lazy { ProgressDialogUtil(requireContext()) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
+    ): View {
+        binding = FragmentEditProfileBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment EditProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            EditProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModel.getSession().observe(viewLifecycleOwner) { session ->
+            userData = UserSession(
+                session.email,
+                session.name,
+                session.password,
+                session.token,
+                session.isLogin
+            )
+
+            getUserData(session.token ?: "")
+        }
+
+
+        setupToolbar()
+        setupAction()
+    }
+
+    private fun setupToolbar() {
+        val toolbar = binding.toolbar
+        toolbar.isTitleCentered = true
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_30)
+        toolbar.setNavigationOnClickListener {
+            findNavController().navigateUp()
+        }
+    }
+
+    private fun setupAction() {
+        binding.btnSave.setOnClickListener {
+            val email = binding.tiEmail.text.toString()
+            val username = binding.tiUsername.text.toString()
+            val password = binding.tiPassword.text.toString()
+            val token = userData.token ?: ""
+
+            if (validateUserData(email, username, password)) {
+                val userData = UserSession(email, username, password, token)
+                viewModel.setUserData(token, userData).observe(viewLifecycleOwner) { result ->
+                    when (result) {
+                        is ResultState.Loading -> {
+                            progressDialog.showLoading()
+                        }
+                        is ResultState.Success -> {
+                            progressDialog.hideLoading()
+                            showToast(getString(R.string.profile_updated))
+                            navigateBack()
+                        }
+                        is ResultState.Error -> {
+                            progressDialog.hideLoading()
+                            showToast(result.error)
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun validateUserData(email: String, username: String, password: String): Boolean {
+        return if (email.isEmpty() || username.isEmpty() || password.isEmpty()) {
+            showToast(getString(R.string.all_fields_must_not_be_empty))
+            false
+        } else {
+            true
+        }
+    }
+
+    private fun getUserData(token: String) {
+        viewModel.getUserData(token)
+            .observe(viewLifecycleOwner) { result ->
+                when (result) {
+                    is ResultState.Loading -> {
+                        progressDialog.showLoading()
+                    }
+
+                    is ResultState.Success -> {
+                        progressDialog.hideLoading()
+                        binding.tiEmail.setText(result.data.user?.email)
+                        binding.tiUsername.setText(result.data.user?.name)
+                    }
+
+                    is ResultState.Error -> {
+                        progressDialog.hideLoading()
+                        val errorMessage = result.error
+                        showToast(errorMessage)
+                    }
                 }
             }
     }
+
+    private fun showToast(toast: String) {
+        Toast.makeText(requireContext(), toast, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun navigateBack() {
+        findNavController().navigateUp()
+    }
+
 }
--- MD/app/src/main/java/com/health/glucoguide/ui/fragment/editprofile/EditProfileViewModel.kt
+++ MD/app/src/main/java/com/health/glucoguide/ui/fragment/editprofile/EditProfileViewModel.kt
@@ -1,4 +1,27 @@
 package com.health.glucoguide.ui.fragment.editprofile
 
-class EditProfileViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.health.glucoguide.data.ResultState
+import com.health.glucoguide.data.UserRepository
+import com.health.glucoguide.models.UserProfileResponse
+import com.health.glucoguide.models.UserSession
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class EditProfileViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+    fun getSession() = userRepository.getSession().asLiveData()
+
+    fun setUserData(token: String, userData: UserSession): LiveData<ResultState<UserProfileResponse>> {
+        return userRepository.setUserData(token, userData)
+    }
+
+    fun getUserData(token: String): LiveData<ResultState<UserProfileResponse>> {
+        return userRepository.getUserData(token)
+    }
 }
