6a6734c4071d428f9ad6f41ff4e590f762d2a708
Restu Gede Purnama <restumelenoy83@gmail.com>
2024-12-05 03:04:02 +0700

    feature/implement get histories from api

--- MD/app/src/main/java/com/health/glucoguide/ui/fragment/history/HistoryFragment.kt
+++ MD/app/src/main/java/com/health/glucoguide/ui/fragment/history/HistoryFragment.kt
@@ -1,21 +1,30 @@
-package com.health.glucoguide.ui.fragment
+package com.health.glucoguide.ui.fragment.history
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.health.glucoguide.adapter.HistoryAdapter
+import com.health.glucoguide.data.ResultState
 import com.health.glucoguide.databinding.FragmentHistoryBinding
+import com.health.glucoguide.models.HistoriesItem
 import com.health.glucoguide.models.UserData
+import com.health.glucoguide.util.ProgressDialogUtil
+import com.health.glucoguide.util.showToast
+import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
+@AndroidEntryPoint
 class HistoryFragment : Fragment() {
     private var _binding: FragmentHistoryBinding? = null
     private val binding get() = _binding!!
+    private val viewModel: HistoryViewModel by viewModels()
+    private val progressDialog by lazy { ProgressDialogUtil(requireContext()) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,9 +37,12 @@ class HistoryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        viewModel.getSession().observe(viewLifecycleOwner) { session ->
+            val token = session.token.toString()
+            getHistories(token)
+        }
+
         setupToolbar()
-        setupRecyclerView()
-        setupData()
     }
 
     private fun setupToolbar() {
@@ -38,26 +50,31 @@ class HistoryFragment : Fragment() {
         toolbar.isTitleCentered = true
     }
 
-    private fun setupRecyclerView() {
-        val users = List(5) {
-            UserData().apply {
-                date = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault()).format(Date())
-                hemoglobinLevel = 12.0
-                glucoseLevel = 120
-            }
-        }
-
+    private fun setupRecyclerView(users: List<HistoriesItem?>?) {
         val adapter = HistoryAdapter()
+
         binding.rvHistory.layoutManager = LinearLayoutManager(context)
         binding.rvHistory.adapter = adapter
         adapter.submitList(users)
     }
 
-    private fun setupData() {
-        val simpleDateFormat = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale.getDefault())
-        val currentDate = Date()
-
-        val user = UserData()
-        user.date = simpleDateFormat.format(currentDate)
+    private fun getHistories(token: String) {
+        viewModel.getHistories(token).observe(viewLifecycleOwner) { result ->
+            when (result) {
+                is ResultState.Loading -> {
+                    progressDialog.showLoading()
+                }
+                is ResultState.Success -> {
+                    progressDialog.hideLoading()
+                    val listHistory = result.data.histories
+                    setupRecyclerView(listHistory)
+                }
+                is ResultState.Error -> {
+                    progressDialog.hideLoading()
+                    val errorMessage = result.error
+                    showToast(errorMessage, requireContext())
+                }
+            }
+        }
     }
 }
