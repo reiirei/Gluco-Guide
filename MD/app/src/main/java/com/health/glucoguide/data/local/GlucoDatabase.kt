72045b8ed54a7d308e25405ea61677aef71939ad
Restu Gede Purnama <restumelenoy83@gmail.com>
2024-12-06 15:58:04 +0700

    feature/added database for implementation offline first app

--- MD/app/src/main/java/com/health/glucoguide/data/local/GlucoDatabase.kt
+++ MD/app/src/main/java/com/health/glucoguide/data/local/GlucoDatabase.kt
@@ -1,4 +1,10 @@
 package com.health.glucoguide.data.local
 
-class GlucoDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.health.glucoguide.models.HistoryEntity
+
+@Database(entities = [HistoryEntity::class], version = 1, exportSchema = false)
+abstract class GlucoDatabase : RoomDatabase() {
+    abstract fun historyDao(): HistoryDao
 }
