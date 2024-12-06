16d112e1357a0d3966f9a2624dfdff339c86a2b7
Restu Gede Purnama <restumelenoy83@gmail.com>
2024-12-06 15:52:32 +0700

    feature/added user entity model for room

--- MD/app/src/main/java/com/health/glucoguide/models/HistoryEntity.kt
+++ MD/app/src/main/java/com/health/glucoguide/models/HistoryEntity.kt
@@ -1,4 +1,20 @@
 package com.health.glucoguide.models
 
-class HIstoryEntity {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "history")
+data class HistoryEntity(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+
+    @ColumnInfo(name = "diagnosa")
+    val diagnosa: String? = null,
+
+    @ColumnInfo(name = "tanggal_cek")
+    val tanggalCek: String? = null,
+
+    @ColumnInfo(name = "keluhan")
+    val keluhan: String? = null
+)
