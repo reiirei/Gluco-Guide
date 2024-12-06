beefde2b731e5da48986ad705ad4415fe4ce00a7
Restu Gede Purnama <restumelenoy83@gmail.com>
2024-12-06 15:36:25 +0700

    feature/added auth interceptor for observe token from api

--- MD/app/src/main/java/com/health/glucoguide/util/AuthInterceptor.kt
+++ MD/app/src/main/java/com/health/glucoguide/util/AuthInterceptor.kt
@@ -1,4 +1,23 @@
 package com.health.glucoguide.util
 
-class AuthInterceptor {
+import android.content.Context
+import android.content.Intent
+import com.health.glucoguide.ui.activity.onboarding.OnBoardingActivity
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor(private val context: Context) : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val response = chain.proceed(request)
+
+        if (response.code == 403) {
+            val intent = Intent(context, OnBoardingActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            }
+            context.startActivity(intent)
+        }
+
+        return response
+    }
 }
