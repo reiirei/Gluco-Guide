681ce0a4fe633bfabbe2caddc08689094911af99
Restu Gede Purnama <restumelenoy83@gmail.com>
2024-12-06 15:44:35 +0700

    added user session model

--- MD/app/src/main/java/com/health/glucoguide/ui/fragment/home/HomeViewModel.kt
+++ MD/app/src/main/java/com/health/glucoguide/ui/fragment/home/HomeViewModel.kt
@@ -3,10 +3,8 @@ package com.health.glucoguide.ui.fragment.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
-import com.health.glucoguide.data.ResultState
 import com.health.glucoguide.data.UserRepository
-import com.health.glucoguide.models.UserProfileResponse
-import com.health.glucoguide.models.UserSession
+import com.health.glucoguide.data.remote.response.UserSession
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
@@ -17,9 +15,5 @@ class HomeViewModel @Inject constructor(
 
     fun getSession(): LiveData<UserSession> {
         return userRepository.getSession().asLiveData()
-    }
-
-    fun getUserData(token: String): LiveData<ResultState<UserProfileResponse>> {
-        return userRepository.getUserData(token)
     }
 }
