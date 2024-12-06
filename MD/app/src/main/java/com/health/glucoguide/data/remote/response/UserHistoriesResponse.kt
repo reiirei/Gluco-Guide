7bbf66f53bafba60bbf129549d4f715df10038df
Restu Gede Purnama <restumelenoy83@gmail.com>
2024-12-06 15:54:55 +0700

    first commit

--- MD/app/src/main/java/com/health/glucoguide/data/remote/response/UserHistoriesResponse.kt
+++ MD/app/src/main/java/com/health/glucoguide/data/remote/response/UserHistoriesResponse.kt
@@ -1,11 +1,11 @@
-package com.health.glucoguide.models
+package com.health.glucoguide.data.remote.response
 
 import com.google.gson.annotations.SerializedName
 
 data class UserHistoriesResponse(
 
 	@field:SerializedName("histories")
-	val histories: List<HistoriesItem?>? = null,
+	val histories: List<HistoriesItem>,
 
 	@field:SerializedName("error")
 	val error: Boolean? = null
