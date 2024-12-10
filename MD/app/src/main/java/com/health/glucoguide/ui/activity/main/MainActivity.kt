fe0abb9022bd94a2e86276a952516dff3a06f608
Restu Gede Purnama <restumelenoy83@gmail.com>
2024-12-11 04:48:49 +0700

    feature/ implement network util and add snackbar for notif

--- MD/app/src/main/java/com/health/glucoguide/ui/activity/main/MainActivity.kt
+++ MD/app/src/main/java/com/health/glucoguide/ui/activity/main/MainActivity.kt
@@ -1,25 +1,30 @@
-package com.health.glucoguide.ui.activity
+package com.health.glucoguide.ui.activity.main
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.snackbar.Snackbar
 import com.health.glucoguide.R
 import com.health.glucoguide.databinding.ActivityMainBinding
-import com.health.glucoguide.ui.fragment.profile.ProfileViewModel
+import com.health.glucoguide.ui.activity.onboarding.OnBoardingActivity
 import com.health.glucoguide.util.LocaleHelper
+import com.health.glucoguide.util.NetworkUtils
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: ProfileViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels()
+    private val networkUtils: NetworkUtils by lazy { NetworkUtils(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +36,43 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        setupObserver()
+
         val language = viewModel.getLanguageSync()
         LocaleHelper.updateLocale(this, language)
 
         setupNavigationController(binding)
+    }
+
+    private fun setupObserver() {
+        networkUtils.observe(this) { isConnected ->
+            if (!isConnected) {
+                showSnackbar(getString(R.string.network_connection_error))
+            } else {
+                showSnackbar(getString(R.string.network_connection_restored), R.color.army)
+            }
+        }
+
+        viewModel.getSession().observe(this) { user ->
+            if (!user.isLogin) {
+                startActivity(Intent(this, OnBoardingActivity::class.java).apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                })
+                finish()
+            }
+        }
+    }
+
+    private fun showSnackbar(errorMessage: String, color: Int = R.color.red) {
+        Snackbar.make(binding.root, errorMessage, Snackbar.LENGTH_SHORT).apply {
+            setBackgroundTint(ContextCompat.getColor(this@MainActivity, color))
+            setTextColor(ContextCompat.getColor(this@MainActivity, R.color.white))
+            setActionTextColor(ContextCompat.getColor(this@MainActivity, R.color.white))
+            anchorView = findViewById(R.id.bottom_navigation)
+            setAction(getString(R.string.ok)) {
+                dismiss()
+            }
+        }.show()
     }
 
     private fun setupNavigationController(binding: ActivityMainBinding) {
